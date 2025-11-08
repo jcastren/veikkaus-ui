b67863e7f90b75c00c51f80b85f2947569c9ecfc
Joonas Castrén <joonas_castren@yahoo.com>
2025-11-08 14:03:52 +0200

    Removed Home from navigation bar

--- my-react-app/src/navigation/NavigationBar.tsx
+++ my-react-app/src/navigation/NavigationBar.tsx
@@ -4,7 +4,6 @@ export default function NavigationBar() {
   return (
     <nav className="bg-gray-100 p-4">
       <ul className="flex space-x-4">
-        <li><Link href="/" className="navigation_item">Home</Link></li>
         <li><Link href="/tournaments" className="navigation_item">Tournaments</Link></li>
         <li><Link href="/teams" className="navigation_item">Teams</Link></li>
         <li><Link href="/tournament-teams" className="navigation_item">Tournament Teams</Link></li>
